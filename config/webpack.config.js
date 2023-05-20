3936f324414be08dba355a16094f253498ebe738
nyako <219716540@qq.com>
2023-05-20 18:14:13 +0800

    fix frame

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -315,7 +315,6 @@ module.exports = function (webpackEnv) {
         .map(ext => `.${ext}`)
         .filter(ext => useTypeScript || !ext.includes('ts')),
       alias: {
-        '@':path.appSrc,
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
