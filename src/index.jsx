e189eb2a76207245f181a441c22fa232bd3fb4ca
nyako <219716540@qq.com>
2023-05-20 19:08:03 +0800

    finish webpack and REM

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 
 import 'lib-flexible'
 import './index.less'
-// css in jsx 不生效
+
+// css in jsx 不生效,webpack里的postcss-pxtorem无法处理。
 // import styled from "styled-components";
 // const StyleProvider = styled.div`
 //     .box{
@@ -15,6 +16,21 @@ import './index.less'
 //         background:lightblue;
 //     }
 // `;
+
+//处理最大宽度
+(function () {
+    const handleMax = () => {
+      let html = document.documentElement,
+        root=document.getElementById('root'),
+        deviceW = html.clientWidth;
+        root.style.maxWidth="750px";
+        if(deviceW>=750){
+            html.style.fontSize = '75px'
+        }
+    };
+    handleMax();
+    window.addEventListener("resize", handleMax);
+  })();
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
