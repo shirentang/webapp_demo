9b32324e47cece304efa242a0ce836564b3c18d7
nyako <219716540@qq.com>
2023-05-31 19:18:35 +0800

    home 3.0

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import "./NewsItem.less";
+import { Image } from "antd-mobile";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-const NewsItem = function NewsItem() {
-  return <div className="news-item-box">单条新闻</div>;
+const NewsItem = function NewsItem(props) {
+  let { info } = props;
+
+  if (!info) return null;
+
+  let { id, title, hint, images } = info;
+  if (!Array.isArray(images)) {
+    images = [""];
+  }
+
+  return (
+    <div className="news-item-box">
+      <Link to={{ pathname: `/detail/${id}` }}>
+        <div className="content">
+          <h4 className="title">{title}</h4>
+          <p className="author">{hint}</p>
+        </div>
+        <Image src={images[0]} lazy />
+      </Link>
+    </div>
+  );
+};
+
+//规则校验
+NewsItem.defaultProps = {
+  info: null,
+};
+
+NewsItem.propTypes = {
+  info: PropTypes.object,
 };
 
 export default NewsItem;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,105 +1,102 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import HomeHead from "../components/HomeHead";
-import _ from '../assets/utils'
-import { Swiper,Image,Divider,DotLoading } from "antd-mobile";
+import _ from "../assets/utils";
+import { Swiper, Image, Divider, DotLoading } from "antd-mobile";
 import { Link } from "react-router-dom";
-import api from '../api'
+import api from "../api";
 import NewsItem from "../components/NewsItem";
-import SkeletonAgain from '../components/SkeletonAgain'
+import SkeletonAgain from "../components/SkeletonAgain";
 
-import './Home.less'
-import thunk from "redux-thunk";
-
-const Home = function Home(){
-    //创建状态
-    let [today,setToday] = useState(_.formatTime(null,"{0}{1}{2}")) ,
-    [bannerData,setBannerData]=useState([]);
-
-    //第一次渲染完后向服务器发送请求
-    useEffect(()=>{
-        (async ()=>{
-            try{
-                let {date,stories,top_stories}=await api.queryNewsLatest()
-                setToday(date)
-                setBannerData(top_stories)
-            }catch(_){
-
-            }
-        })()
-    },[])
+import "./Home.less";
 
 
-    return (
-        <div className="home-box">
-            {/* 头部 */}
-            <HomeHead today={today}/>
+const Home = function Home() {
+  //创建状态
+  let [today, setToday] = useState(_.formatTime(null, "{0}{1}{2}")),
+    [bannerData, setBannerData] = useState([]),
+    [newsList, setNewsList] = useState([]);
 
-            {/* 轮播图 */}
-            <div className="swiper-box">
-                {bannerData.length>0?<Swiper autoplay={true} loop={true}>
-                    {bannerData.map(item=>{
-                        let {id,image,title,hint}=item
-                        return <Swiper.Item key={id}>
-                        <Link to={{
-                            pathname:`/detail/${id}`
-                        }}>
-                            <Image src={image} lazy/>
-                            <div className="desc">
-                                <h3 className="title">{title}</h3>
-                                <p className="author">{hint}</p>
-                            </div>
-                        </Link>
-                    </Swiper.Item>
-                    })}
-                </Swiper> : null}                
-            </div>
+  //第一次渲染完后向服务器发送请求
+  useEffect(() => {
+    (async () => {
+      try {
+        let { date, stories, top_stories } = await api.queryNewsLatest();
+        setToday(date);
+        setBannerData(top_stories);
 
-            {/* 新闻列表 */}
-            <SkeletonAgain/>
+        newsList.push({
+          date,
+          stories,
+        });
+        setNewsList([...newsList]);
+      } catch (_) {}
+    })();
+  }, []);
 
-            <div className="news-box">
-                <Divider contentPosition="left">
-                    12月23日
-                </Divider>
+  //处理时间
+
+  return (
+    <div className="home-box">
+      {/* 头部 */}
+      <HomeHead today={today} />
+
+      {/* 轮播图 */}
+      <div className="swiper-box">
+        {bannerData.length > 0 ? (
+          <Swiper autoplay={true} loop={true}>
+            {bannerData.map((item) => {
+              let { id, image, title, hint } = item;
+              return (
+                <Swiper.Item key={id}>
+                  <Link
+                    to={{
+                      pathname: `/detail/${id}`,
+                    }}
+                  >
+                    <Image src={image} lazy />
+                    <div className="desc">
+                      <h3 className="title">{title}</h3>
+                      <p className="author">{hint}</p>
+                    </div>
+                  </Link>
+                </Swiper.Item>
+              );
+            })}
+          </Swiper>
+        ) : null}
+      </div>
+
+      {/* 新闻列表 */}
+      {newsList.length === 0 ? (
+        <SkeletonAgain />
+      ) : (
+        <>
+          {newsList.map((item, index) => {
+            let { date, stories } = item;
+            return (
+              <div className="news-box" key={date}>
+                {index !== 0 ? (
+                  <Divider contentPosition="left">
+                    {_.formatTime(date, "{1}月{2}日")}
+                  </Divider>
+                ) : null}
                 <div className="list">
-                    <NewsItem />
-                    <NewsItem />
-                    <NewsItem />
-                    <NewsItem />
+                  {stories.map((cur) => {
+                    return <NewsItem key={cur.id} info={cur} />;
+                  })}
                 </div>
-            </div>
-            
-            <div className="news-box">
-                <Divider contentPosition="left">
-                    12月23日
-                </Divider>
-                <div className="list">
-                    <NewsItem />
-                    <NewsItem />
-                    <NewsItem />
-                    <NewsItem />
-                </div>
-            </div>
+              </div>
+            );
+          })}
+        </>
+      )}
 
-            <div className="news-box">
-                <Divider contentPosition="left">
-                    12月23日
-                </Divider>
-                <div className="list">
-                    <NewsItem />
-                    <NewsItem />
-                    <NewsItem />
-                    <NewsItem />
-                </div>
-            </div>
-            
-            {/* 加载更多的盒子（intersectionobsever） */}
-            <div className="loadmore-box">
-                <DotLoading />
-                数据加载中
-            </div>
-
-        </div>
-    )
+      {/* 加载更多的盒子（intersectionobsever） */}
+      <div className="loadmore-box">
+        <DotLoading />
+        数据加载中
+      </div>
+    </div>
+  );
 };
 export default Home;
